fe48eb7bec84491e37ebb137ca41e24aed2a2318
Bermejo246 <101293603+Bermejo246@users.noreply.github.com>
2022-12-02 10:30:16 -0600

    change

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Display Time
+// Time
 
 var today = moment();
 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 
-// Change Background Color
+// Background Color 
 
 
 var timeActual = today.format("H");
@@ -18,18 +18,6 @@ console.log(timeCal)
 
 console.log(timeNumber)
 
-
-// for (let i = 0; i < timeCal.length; i++) {
-//    return (timeNumber);
-// }
-
-// if (timeNumber < timeActual) {
-   
-// }else if (timeNumber === timeActual) {
-
-// }else (timeNumber > timeActual); {
-
-// };
 
 
 var taskInputEl = $('#formGroupExampleInput')
